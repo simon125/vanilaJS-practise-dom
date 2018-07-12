a687a620ea579af22d33209760c6f54e4d790c07
Szymon Oleszek <szymqwerty@gmail.com>
2018-07-12 18:32:52 +0200

    added function for saving list to local storage

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,21 @@
 const form = document.getElementById('addForm')
-const itemList = document.getElementById('items')
+const itemList = document.querySelector('#items')
 const filter = document.getElementById('filter')
+const saveButton = document.querySelector('#saveButton')
 
-
-//FOMR SUBMIT EVENT
+const createListItemFromLocalStorage = () => {
+    const itemListFromLS = JSON.parse(localStorage.getItem('array'))
+    const countOfProperties = Object.keys(itemListFromLS).length
+    const ul = document.querySelector('#items')
+    for (let i = 0; i < countOfProperties; i++) {
+        const li = document.createElement('li')
+        li.classList = 'list-group-item'
+        li.innerHTML = `${itemListFromLS[i]}  <button class="btn btn-danger btn-sm float-right delete">X</button>`
+        ul.appendChild(li)
+    }
+    console.log(ul)
+    return ul
+}
 const addItem = (e) => {
     e.preventDefault()
     const newItem = document.getElementById('item').value
@@ -27,21 +39,36 @@ const removeItem = (e) => {
             itemList.removeChild(li)
         }
     }
-    console.log(1)
 }
-
 const filterItems = (e) => {
     const text = e.target.value.toLowerCase()
     const items = itemList.getElementsByTagName('li')
     console.log(items)
     Array.from(items).forEach((item) => {
         const itemName = item.firstChild.textContent
-        if(itemName.toLowerCase().indexOf(text) !== -1){
+        if (itemName.toLowerCase().indexOf(text) !== -1) {
             item.style.display = 'block'
-        }else item.style.display = 'none'
+        } else item.style.display = 'none'
     })
 }
+const createListElementJSON = () => {
+    const items = itemList.getElementsByTagName('li')
+    const obj = {}
 
+    Array.from(items).forEach((el, i, arr) => {
+        obj[i] = el.innerText.slice(0, el.innerText.length - 1)
+    })
+    return JSON.stringify(obj)
+}
+const saveItemList = (e) => {
+    localStorage.clear()
+    localStorage.setItem('array', createListElementJSON())
+}
+const displaySavedList = () => {
+
+}
+
+saveButton.addEventListener('click', saveItemList)
 form.addEventListener('submit', addItem)
 itemList.addEventListener('click', removeItem)
 filter.addEventListener('keyup', filterItems)
